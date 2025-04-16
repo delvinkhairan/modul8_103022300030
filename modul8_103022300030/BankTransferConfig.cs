5f2406495f07fd6dd2d532175b47fc0183b27423
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 17:02:10 +0700

    add BankTransferConfig.cs

--- modul8_103022300030/BankTransferConfig.cs
+++ modul8_103022300030/BankTransferConfig.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Loader;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace modul8_103022300030
 {
-    internal class BankTransferConfig
+     class BankTransferConfig
     {
+        //Attribute untuk diserialisasi
+        public int lang { get; set; }
+        public int transfer { get; set; }
+        //Constructor kosong untuk deserialisasi
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, int transfer)
+        {
+            lang = lang;
+            transfer = transfer;
+        }
+
     }
+
+    class UIConfig
+    {
+        public BankTransferConfig config;
+        public const String filePath = @"config.json";
+
+        public int Lang { get; private set; }
+        public int Transfer { get; private set; }
+
+        public UIConfig() { }
+        private void SetDefault() {
+            Console.WriteLine("ENG = Select transfer method:");
+            Console.WriteLine("ID = Pilih metode transfer:");
+        }
+        private void WriteNewConfigFile() { }
+
+        private BankTransferConfig ReadConfigFie()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+            return config;
+        }
+
+        private void WriteNewConfigFiled()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Membuat object konfigurasi
+            UIConfig uIConfig = new UIConfig();
+            //Membaca data konfigurasi untuk digunakan untuk setting
+            Lang = uIConfig.config.lang;
+            Transfer = uIConfig.config.transfer;
+        }
+
+    }
+
+
 }
+
+
+
--- modul8_103022300030/Program.cs
+++ modul8_103022300030/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using modul8_103022300030;
+
+class Program
+{
+    static void Main(String[] args) 
+    {
+        BankTransferConfig config = new BankTransferConfig();
+
+        int input;
+
+        if (input == 1)
+        {
+        
+        }
+
+        Console.WriteLine("1. EN = Select transfer method: ");
+        Console.WriteLine("2. ID = Pilih metode transfer:");
+
+
+
+    }
+}
+
